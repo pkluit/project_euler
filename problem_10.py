7848a5a597a6594546b422d7f04dad50c387edee
Kluitenberg, Paul <paul.kluitenberg@gmail.com>
2020-02-04 20:27:14 -0600

    PK: problem_10 completed

--- problem_10.py
+++ problem_10.py
@@ -1,27 +1,24 @@
 ########################################################################################################################
-# Title:    problem_9.py                                                                                               #
+# Title:    problem_10.py                                                                                              #
 # Purpose:  The purpose of this script is to solve the following problem                                               #
-# Problem:  There exists exactly one Pythagorean triplet for which a + b + c = 1000.                                   #
-#           Find the product abc.                                                                                      #
+# Problem:  Find the sum of all the primes below two million.                                                          #
 # Last Modified; 2/4/2020                                                                                              #
 # Property of: Paul Kluitenberg                                                                                        #
 ########################################################################################################################
 
 #=============================================Import Packages==========================================================#
 
-
+from problem_3 import is_prime
 
 #=============================================Define Functions=========================================================#
 
-def criteria_met(a: int,b: int, c: int, sum: int):
-    if ((a+b+c)==sum) and (a < b < c) and ((a**2+b**2)==c**2):
-        return True
-    else:
-        return False
+
 
 #=============================================Output Solution==========================================================#
 
-SUM = 1000
-val_range = range(0,SUM+1)
-lst=[[a,b,c,a*b*c] for a in val_range for b in val_range for c in val_range if criteria_met(a,b,c,SUM)]
-solution = lst[3]
+limit = 2000000
+prime_lst = [2]
+val_range = range(3,limit,2)
+[prime_lst.append(x) for x in val_range if is_prime(x)]
+solution = sum(prime_lst)
+print(solution)
